9cc7dd1b0bf19523ac9c867f231399653bf3bf9e
ftorrecillas <dam.ptorrecillas@institutoserpis.org>
2015-10-07 08:53:42 +0200

    Hola mundo commit desde eclipse

--- JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -3,7 +3,10 @@ package org.institutoserpis.ed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hola mundo desde eclipse");
+		System.out.println("Hola Pablo desde eclipse");
+		
 	}
 
 }
